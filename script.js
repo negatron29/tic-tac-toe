065b22fa02e4c8079966be577566bb123e3f8bbc
negatron29 <giancarlo.cusano@icloud.com>
2020-05-27 21:37:41 -0400

    figured out hhow to determine winner

--- script.js
+++ script.js
@@ -17,12 +17,18 @@ const gameBoard = (() => {
             //console.log(newDiv);
         }
     };
-    return {render};
-        
-    })();
 
-const gameController = () => {
-    const test = "Hey!";
+    const markBoard = (sqId,thisMark) => {
+        idStr = sqId.toString();
+        let box = document.getElementById(idStr);
+        if (box.innerHTML === "") {
+            box.innerHTML = thisMark;
+        }
+    }
+    return {render,markBoard};    
+    })();//This makes an instance of itself when the command runs
+
+const gameController = (() => {
     const clearBoard = () => {
         let sqCnt = 9;
         for (let i = 0; i < sqCnt; i++) {
@@ -31,22 +37,61 @@ const gameController = () => {
             tgtDiv.innerHTML = "";
     }
 }
-return {clearBoard};
-}
+    const recordMark = (playerId,sqId) => {
+        scoreBook[playerId].push(sqId);
+    }
+
+    const checkWinner = (playerId) => {
+        let winningArrays = [
+            ["0","1","2"],
+            ["3","4","5"],
+            ["6","7","8"],
+            ["0","3","6"],
+            ["1","4","7"],
+            ["2","5","8"],
+            ["0","4","8"],
+            ["2","4","6"]
+        ]
+        let playerMarks = scoreBook[playerId];
+        let won = 0;
+        for (let i = 0; i < winningArrays.length; i++) {
+            //do somethiing
+            let valuesFound = 0;
+            for (let j = 0; j < playerMarks.length; j++) {
+                if (winningArrays[i].includes(playerMarks[j]) === true) {
+                    valuesFound +=1;
+                }
+            }
+            if (valuesFound === 3) {
+                won +=1;
+            }
+        }
+        if (won > 0) {
+            return "Y";
+        }
+        else {
+            return "N"
+        }
+    }
+
+return {clearBoard,recordMark,checkWinner};
+})();
+
+const scoreBook = [
+    [],[]
+];
 
 const Player = (name) => {
     let thisMark = allMarks[currentMark];
     const markSpot = (sqId) => {
-        idStr = sqId.toString();
-        let box = document.getElementById(idStr);
-        if (box.innerHTML === "") {
-            box.innerHTML = thisMark;
-        }
+        gameBoard.markBoard(sqId,thisMark);
+
     }
     currentMark +=1;
     return {name,markSpot};
     
 }
+
 
 //This object/function will check for a winner 
 //after every "mark" is made
